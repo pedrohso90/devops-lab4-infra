7ed32a3a12bdc48cea3552e55b1cd9e29f48c76f
Pedro Oliveira <pedoliveira@uolinc.com>
2022-03-05 11:37:59 -0300

    first commit

--- gcp-main.tf
+++ gcp-main.tf
@@ -10,6 +10,7 @@ resource "google_sql_database_instance" "db_pedroh" {
 resource "google_sql_user" "db_pedroh" {
   name     = var.username
   instance = google_sql_database_instance.db_pedroh.name
-  host     = "me.com"
   password = var.password
 }
+
+output "public_ip_address " {value=google_sql_database_instance.db_pedroh.public_ip_address}
